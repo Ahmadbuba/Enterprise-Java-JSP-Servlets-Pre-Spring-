e9b9e666877ae2c102200bbe4dee5c0de03b56a9
AhmadBuba <ahmadbubacherryfield@gmail.com>
2022-12-08 14:12:13 +0100

    feat - completed the MVC Student tracker app consisting of full CRUD features

--- web-student-tracker/src/com/luv2code/web/jdbc/StudentControllerServlet.java
+++ web-student-tracker/src/com/luv2code/web/jdbc/StudentControllerServlet.java
@@ -44,12 +44,118 @@ public class StudentControllerServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		try {
-		// list the students ... in MVC fashion
-		listStudents(request,response);
+			// read the "command" parameter
+			String theCommand = request.getParameter("command");
+			
+			// if the command is missing, then default to listing students
+			if (theCommand == null) {
+				theCommand = "LIST";
+			}
+			
+			// route to the appropriate method
+			switch (theCommand) {
+			case "LIST":
+				// list the students ... in MVC fashion
+				listStudents(request,response);
+				break;
+				
+			case "ADD":
+				addStudent(request,response);
+				break;
+			
+			case "LOAD":
+				loadStudent(request, response);
+				break;
+				
+			case "UPDATE":
+				updateStudent(request, response);
+				break;
+				
+			case "DELETE":
+				deleteStudent(request, response);
+				break;
+				
+			default:
+				listStudents(request,response);
+			}
+			
+			
 		}
 		catch (Exception e) {
 			throw new ServletException(e);
 		}
+	}
+
+
+
+	private void deleteStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		// read student info from form data
+		int theStudentId = Integer.parseInt(request.getParameter("studentId"));
+				
+		// delete student from database
+		studentDbUtil.deleteStudent(theStudentId);
+		
+		// send them back to "list students" page
+		listStudents(request, response);
+	}
+
+
+
+	private void updateStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		// read student info from form data
+		int id = Integer.parseInt(request.getParameter("studentId"));
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		
+		// create a new student object
+		Student theStudent = new Student(id, firstName, lastName, email);
+		
+		// perform update on database
+		studentDbUtil.updateStudent(theStudent);
+		
+		// send them back to the "listing student" page
+		listStudents(request, response);
+	}
+
+
+
+	private void loadStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		// read student if from form data
+		String theStudentId = request.getParameter("studentId");
+		
+		// get student from database (db util)
+		Student theStudent = studentDbUtil.getStudent(theStudentId);
+		
+		// place student in the request attribute
+		request.setAttribute("THE_STUDENT", theStudent);
+		
+		// send to jsp page: update-student-form.jsp
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/update-student-form.jsp");
+		dispatcher.forward(request, response);
+	}
+
+
+
+	private void addStudent(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		// read student info from form data
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		
+		// create a new student object
+		Student theStudent = new Student(firstName, lastName, email);
+		
+		// add the student to the database
+		studentDbUtil.addStudent(theStudent);
+		
+		// send back to main page (the student list)
+		listStudents(request,response);
+		
 	}
 
 
--- web-student-tracker/src/com/luv2code/web/jdbc/StudentDbUtil.java
+++ web-student-tracker/src/com/luv2code/web/jdbc/StudentDbUtil.java
@@ -1,7 +1,9 @@
 package com.luv2code.web.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -79,4 +81,148 @@ public class StudentDbUtil {
 		}
 		
 	}
+
+	public void addStudent(Student theStudent) throws Exception {
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		
+		try {
+			// get db connection 
+			myConn = dataSource.getConnection();
+			
+			// create sql for insert
+			String sql = "insert into student " 
+					+ "(first_name, last_name, email) "
+					+ "values (?, ?, ?)";
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set the param values for the student
+			myStmt.setString(1, theStudent.getFirstName());
+			myStmt.setString(2, theStudent.getLastName());
+			myStmt.setString(3, theStudent.getEmail());
+			
+			// execute sql insert
+			myStmt.execute();
+			}
+			finally {
+				// clean up JDBC objects
+				close(myConn,myStmt,null);
+			}
+	}
+
+	public Student getStudent(String theStudentId) throws Exception {
+		
+		Student theStudent = null;
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		ResultSet myRs = null;
+		int studentId;
+		
+		try {
+			// convert student id to int
+			studentId = Integer.parseInt(theStudentId);
+			
+			// get connection to database
+			myConn = dataSource.getConnection();
+			
+			// create sql to get selected student
+			String sql = "select * from student where id=?";
+			
+			// create prepared statement
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set params
+			myStmt.setInt(1, studentId);
+			
+			// execute statement
+			myRs = myStmt.executeQuery();
+			
+			// retrieve data from result set row
+			if (myRs.next()) {
+				String firstName = myRs.getString("first_name");
+				String lastName = myRs.getString("last_name");
+				String email = myRs.getString("email");
+				
+				// use the studentId during construction
+				theStudent = new Student (studentId, firstName, lastName, email);
+			}
+			else {
+				throw new Exception("Could not find student id: " + studentId);
+			}
+			return theStudent;
+		}
+		finally {
+			// clean up JDBC objects
+			close(myConn,myStmt,myRs);
+		}
+		
+	}
+
+	public void updateStudent(Student theStudent) throws Exception {
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		
+		try {
+			// get db connection
+			myConn = dataSource.getConnection();
+			
+			// create SQL update statement
+			String sql = "update student "
+						+ "set first_name=?, last_name=?, email=? "
+						+ "where id=?";
+			
+			// prepare statement
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set params
+			myStmt.setString(1, theStudent.getFirstName());
+			myStmt.setString(2, theStudent.getLastName());
+			myStmt.setString(3, theStudent.getEmail());
+			myStmt.setInt(4, theStudent.getId());
+			
+			// execute SQL statement
+			myStmt.execute();
+		}
+		finally {
+			// clean up JDBC objects
+			close(myConn, myStmt, null);
+		}
+		
+	}
+
+	public void deleteStudent(int theStudentId) throws Exception {
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		
+		try {
+			// convert student id to int
+			int studentId = theStudentId;
+			
+			// get connection to database
+			myConn = dataSource.getConnection();
+			
+			// create sql to delete student
+			String sql = "delete from student where id=?";
+			
+			// prepare statement
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set params 
+			myStmt.setInt(1, studentId);
+			
+			// execute sql statement
+			myStmt.execute();
+		}
+		finally {
+			// clean up JDBC objects
+			close(myConn, myStmt, null);
+		}
+		
+	}
+		
+		
 }
